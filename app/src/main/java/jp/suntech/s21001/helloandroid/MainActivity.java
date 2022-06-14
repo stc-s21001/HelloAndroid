944392e6b6b1cde70bcd7fc58dfc449d7b623c63
s21001 <s21001@suntech.jp>
2022-06-14 11:36:10 +0900

    Second Commit

--- app/src/main/java/jp/suntech/s21001/helloandroid/MainActivity.java
+++ app/src/main/java/jp/suntech/s21001/helloandroid/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //2回目
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
